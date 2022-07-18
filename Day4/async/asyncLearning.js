c8fff4217d9a3bd8a0626a34de97c5d217911a44
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-18 10:18:40 +0300

    EventLoop

--- Day4/async/asyncLearning.js
+++ Day4/async/asyncLearning.js
@@ -53,26 +53,26 @@
 
 
 
-// function myDisplayer(some) {
-//   document.getElementById("demo").innerHTML = some;
-// }
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
 
-// let myPromise = new Promise(function(myResolve, myReject) {
-//   let x = 0;
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
 
-// // The producing code (this may take some time)
+// The producing code (this may take some time)
 
-//   if (x == 0) {
-//     myResolve("OK");
-//   } else {
-//     myReject("Error");
-//   }
-// });
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
 
-// myPromise.then(
-//   function(value) {myDisplayer(value);},
-//   function(error) {myDisplayer(error);}
-// );
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
 
 
 
