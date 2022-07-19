179a411cee2595f511957ca182322d1c1b014dec
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-19 15:59:46 +0300

    axios usage

---
+++ Day6/AxiosExample/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import {
+  View,
+  FlatList,
+  Text
+} from 'react-native';
+import ProductCard from './Component/ProductCard/ProductCard';
+import axios from 'axios';
+
+
+const App = () => {
+  const [list,setList]=useState("");
+  const baseURL="https://jsonplaceholder.typicode.com/photos";
+  const rendered= ({item})=>(<ProductCard product={item}/>)
+  
+  
+    axios
+    .get(`${baseURL}`)
+    .then(function (response) {
+     setList(response.data)
+     console.log(response.data)
+    })
+    .catch(function (error) {
+      console.log(`${error} Bir Hata Oluştu`);
+    });
+  
+  
+
+  return (
+    <View >
+  
+    <FlatList
+    keyExtractor={item=>item.id}
+    data={list}
+    renderItem ={rendered}
+    />
+    </View>
+  );
+};
+
+
+export default App;
---
+++ Day6/AxiosExample/src/Component/ProductCard/ProductCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Image, Text, View,TouchableOpacity} from 'react-native';
+import styles from './ProductCard.style';
+
+const ProductCard = ({product}) => {
+  return (
+    <View style={styles.container} >
+        <Image
+        style={styles.image}
+        source={{uri: product.url}}
+      />  
+      <View style={styles.inner_container}>
+        <Text style={styles.name}>{product.id}</Text>
+        <Text style={styles.price}>{product.title}</Text>
+      </View>
+      {/* <TouchableOpacity
+        style={styles.buyButton}
+        onPress={()=>{console.log("Sepete Eklendi")}}
+      >
+        <Text style={styles.buyText}>Sepete Ekle</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
+};
+
+export default ProductCard;
